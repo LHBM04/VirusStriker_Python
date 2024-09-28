e43a00c4593712bb3cf086690a49f1bcd634b7c8
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-09-28 23:22:49 +0900

    feat : Resource 뱅크 시스템 작성 #1

--- Frameworks/Utilities/Resource.py
+++ Frameworks/Utilities/Resource.py
@@ -1,16 +1,41 @@
 from pico2d import *
 from pydub import AudioSegment
 
-g_bgmBank = { str : AudioSegment }
-g_sfxBank = { str : AudioSegment }
+g_sprite_bank = { str : Image } # 스프라이트 뱅크.
 
-def LoadBGM(_path: str, _name: str) -> None:
-    if _path in g_bgmBank.keys():
-        print(f"{_name} is already loaded.")
+def add_sprite(_path: str) -> None:
+    global g_sprite_bank
+    if _path in g_sprite_bank.keys():
+        print(f"{os.path.basename(_path)} is already loaded.")
         return
+    
+    g_sprite_bank[_path] = load_image(_path)
 
+g_bgm_bank = { str : AudioSegment }  # BGM 뱅크.
+g_sfx_bank = { str : AudioSegment }  # SFX 뱅크.
+
+def add_bgm(_path: str) -> None:
+    global g_bgm_bank
+    if _path in g_bgm_bank.keys():
+        print(f"{os.path.basename(_path)} is already loaded.")
+        return
+    
     try:
-        g_bgmBank[_name] = AudioSegment.from_wav(_path)
-        print(f"{_name} loaded successfully from {_path}.")
+        g_bgm_bank[_path] = AudioSegment.from_wav(_path)
+    except FileNotFoundError as e:
+        assert(e)
     except Exception as e:
-        print(f"Error loading {_name} from {_path}: {e}")
+        assert(e)
+
+def add_sfx(_path: str) -> None:
+    global g_sfx_bank
+    if _path in g_sfx_bank.keys():
+        print(f"{os.path.basename(_path)} is already loaded.")
+        return
+    
+    try:
+        g_bgm_bank[_path] = AudioSegment.from_wav(_path)
+    except FileNotFoundError as e:
+        assert(e)
+    except Exception as e:
+        assert(e)
