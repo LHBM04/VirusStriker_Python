0ff63efe3d1aae832036a1a4b966abadc4eb9942
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-09-30 06:37:52 +0900

    feat : Singleton 패턴 재추가

---
+++ Frameworks/Utilities/Singleton.py
@@ -0,0 +1,7 @@
+class Singleton(type):
+    g_instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls.g_instances:
+            instance = super().__call__(*args, **kwargs)
+            cls.g_instances[cls] = instance
+        return cls.g_instances[cls]
