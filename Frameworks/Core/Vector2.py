fe779e3bee9169de9f69e36687fd0a5284db1c11
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-09-30 00:34:40 +0900

    feat : Vector2 클래스 작성

--- Frameworks/Core/Vector2.py
+++ Frameworks/Core/Vector2.py
@@ -1,7 +1,35 @@
-class Vector2:
-    m_x: float
-    m_y: float
+import numpy as np
 
-    def __init__(self, _x: float, _y: float) -> None:
-        self.m_x = _x
-        self.m_y = _y
+from Utilities.MathF import *
+
+class Vector2:
+    def __init__(self, _x: float = 0, _y: float = 0) -> None:
+        self.m_x: float = _x
+        self.m_y: float = _y
+
+    def __neg__(self) -> 'Vector2':
+        return Vector2(-self.x, -self.y)
+
+    def __add__(self, _other: 'Vector2') -> 'Vector2':
+        return Vector2(self.x + _other.x, self.y + _other.y)
+
+    def __sub__(self, _other: 'Vector2') -> 'Vector2':
+        return Vector2(self.x - _other.x, self.y - _other.y)
+
+    def __mul__(self, _other: 'Vector2') -> 'Vector2':
+        return Vector2(self.x * _other, self.y * _other)
+
+    def __truediv__(self, _other: 'Vector2') -> 'Vector2':
+        return Vector2(self.x / _other, self.y / _other)
+
+    def __floor__(self) -> 'Vector2':
+        return Vector2(MathF.Floor(self.x), MathF.Floor(self.y))
+
+    def __ceil__(self) -> 'Vector2':
+        return Vector2(MathF.Ceil(self.x), MathF.Ceil(self.y))
+
+    def magnitude(self) -> float:
+        return np.sqrt(self.m_x ** 2 + self.m_y ** 2)
+    
+    def dot(self, _other: 'Vector2') -> float:
+        return np.dot([self.m_x, _other.m_y], [self.m_x, _other.m_y])
