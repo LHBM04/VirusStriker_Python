55a26b66b320decefcbc377af4ee53f6d256ce85
WHTBUM <bum2680@naver.com>
2024-09-29 19:43:18 +0900

    fefactor : 클래스 이름 변경

--- Frameworks/Utilities/FileSystem.py
+++ Frameworks/Utilities/FileSystem.py
@@ -2,19 +2,19 @@ from pico2d import *
 
 g_sprite_bank = { str : Image } # 스프라이트 뱅크.
 
-def AddSprite(_path: str):
+def AddSprite(_filePath: str):
     try:
-        if _path in g_sprite_bank.keys():
+        if _filePath in g_sprite_bank.keys():
             return
     
-        g_sprite_bank[_path] = load_image(str)
+        g_sprite_bank[_filePath] = load_image(str)
     except FileNotFoundError:
-        print(f"[Oops!] 파일을 찾지 못했습니다. 검색한 경로는 \"{_path}\"였습니다.")
+        print(f"[Oops!] 파일을 찾지 못했습니다. 검색한 경로는 \"{_filePath}\"였습니다.")
     except Exception as exception:
         print(f"[Oops!] 파일을 불러오는 중 오류가 발생했습니다. :{exception}")
 
-def GetSprite(_key: str):
-    if _key not in g_sprite_bank.keys():
-        AddSprite(_key)
+def GetSprite(_filePath: str):
+    if _filePath not in g_sprite_bank.keys():
+        AddSprite(_filePath)
 
-    return g_sprite_bank[_key]
+    return g_sprite_bank[_filePath]
