56da44b2567f3bf4ed9ff14d2988a76f0e4bb343
WHTBUM <bum2680@naver.com>
2024-10-08 15:07:00 +0900

    refactor : 좌표 갱신 시 자식 오브젝트 트랜스폼 정보 갱신 추가

--- Scripts/Frameworks/Core/Components/Transform.py
+++ Scripts/Frameworks/Core/Components/Transform.py
@@ -1,36 +1,35 @@
 from typing import List, Union
 
 from Core.Components.Component import Component
+from Core.Components.GameObject import GameObject
 from Core.Utilities.Mathematics import Vector2, Vector3
 
+# 객체의 위치, 크기, 회전값 등의 정보를 담고 있습니다.
 class Transform(Component):
-    from Core.Components.GameObject import GameObject
     def __init__(self, _owner: GameObject):
         super().__init__(_owner)
 
-        self.__position: Vector2  = Vector2()   # 포지션
-        self.__scale: Vector2     = Vector2()   # 크기
-        self.__rotation: float    = 0           # 회전 각도(Z축)
+        self.__position: Vector2            = Vector2()   # 포지션
+        self.__scale: Vector2               = Vector2()   # 크기
+        self.__rotation: float              = 0           # 회전 각도(Z축)
 
-        self.__parent: 'Transform'          = None  # 부모 Transform
-        self.__children: List['Transform']  = []    # 자식 Transform
+        self.__parent: 'Transform'          = None        # 부모 Transform
+        self.__children: List['Transform']  = []          # 자식 Transform
 
     # region [Properties]
-    # 해당 Transform의 Position을 가져옵니다.
     @property
     def position(self) -> Vector2:
         return self.__position
 
-    # 해당 Transform의 Position을 설정합니다.
     @position.setter
     def position(self, _position: Union[Vector2, Vector3]) -> None:
         if isinstance(_position, Vector2):
             self.__position = Vector2(_position.x, _position.y)
         elif isinstance(_position, Vector3):
-            self.__position = _position
+            self.__position = Vector2(_position.x, _position.y)  # Z축은 무시
         else:
-            raise ValueError(f"[Oops!] 매개변수가 Vector2, Vector3가 아닙니다! " +
-                             f"매개변수는 {type(_position)}이었습니다.")
+            raise ValueError(f"[Oops!] 매개변수가 Vector2, Vector3가 아닙니다! 매개변수는 {type(_position)}이었습니다.")
+        self.__UpdateChildren()
 
     @property
     def localPosition(self) -> Vector2:
@@ -44,24 +43,22 @@ class Transform(Component):
             self.__position = _localPosition + self.__parent.position
         else:
             self.__position = _localPosition
+        self.__UpdateChildren()
 
-    # 해당 Transform의 Scale을 가져옵니다.
     @property
     def scale(self) -> Vector2:
         return self.__scale
 
-    # 해당 Transform의 Scale을 설정합니다.
     @scale.setter
     def scale(self, _scale: Union[Vector2, Vector3]) -> None:
         if isinstance(_scale, Vector2):
-            self.__position = Vector2(_scale.x, _scale.y)
+            self.__scale = Vector2(_scale.x, _scale.y)
         elif isinstance(_scale, Vector3):
-            self.__position = _scale
+            self.__scale = Vector2(_scale.x, _scale.y)  # Z축은 무시
         else:
-            raise ValueError(f"[Oops!] 매개변수가 Vector2, Vector3가 아닙니다!" +
-                             f"매개변수는 {type(_scale)}이었습니다.")
+            raise ValueError(f"[Oops!] 매개변수가 Vector2, Vector3가 아닙니다! 매개변수는 {type(_scale)}이었습니다.")
+        self.__UpdateChildren()
 
-    # 로컬 스케일 가져오기 및 설정
     @property
     def localScale(self) -> Vector2:
         if self.__parent is not None:
@@ -74,24 +71,20 @@ class Transform(Component):
             self.__scale = Vector2(_localScale.x * self.__parent.scale.x, _localScale.y * self.__parent.scale.y)
         else:
             self.__scale = _localScale
+        self.__UpdateChildren()
 
-    # 해당 Transform의 Rotation을 가져옵니다.
     @property
     def rotation(self) -> float:
         return self.__rotation
 
-    # 해당 Transform의 Rotation을 설정합니다.
     @rotation.setter
-    def rotation(self, _rotation: Union[Vector2, Vector3]) -> None:
-        if isinstance(_rotation, Vector2):
-            self.__position = Vector2(_rotation.x, _rotation.y)
-        elif isinstance(_rotation, Vector3):
-            self.__position = _rotation
+    def rotation(self, _rotation: float) -> None:
+        if isinstance(_rotation, (float, int)):
+            self.__rotation = _rotation
         else:
-            raise ValueError(f"[Oops!] 매개변수가 Vector2, Vector3가 아닙니다!" +
-                             f"매개변수는 {type(_rotation)}이었습니다.")
+            raise ValueError(f"[Oops!] 회전 값은 float 또는 int여야 합니다! 매개변수는 {type(_rotation)}이었습니다.")
+        self.__UpdateChildren()
 
-    # 로컬 회전(Z축만) 가져오기 및 설정
     @property
     def localRotation(self) -> float:
         if self.__parent is not None:
@@ -104,42 +97,25 @@ class Transform(Component):
             self.__rotation = _localRotation + self.__parent.rotation
         else:
             self.__rotation = _localRotation
+        self.__UpdateChildren()
 
-    # 해당 Transform의 부모 Transform을 가져옵니다. (Read-Only)
     @property
     def parent(self) -> 'Transform':
         return self.__parent
 
-    @parent.setter
     def parent(self, _parent: 'Transform') -> None:
         self.__parent = _parent
-        _parent.children.append(self)
+        _parent.__children.append(self)
+        self.__UpdateChildren()
 
-    # 해당 Transform의 자식 Transform을 가져옵니다. (Read-Only)
     @property
-    def children(self) -> List['Transform']:
+    def children(self) -> list['Transform']:
         return self.__children
     # endregion
-    # region [Methods]
-    # 해당 인덱스에 있는 자식 Transform을 가져옵니다.
-    def GetChild(self, _index: Union[int, str]) -> 'Transform':
-        if isinstance(_index, int):
-            if (_index < 0 or
-                    _index >= len(self.__children)):
-                raise ValueError(f"[Oops!] 인덱스가 해당 범위를 벗어났습니다.")
 
-            return self.__children[_index]
-        elif isinstance(_index, str):
-            if (_index is None or
-                    _index == ""):
-                raise ValueError(f"[Oops!] 인덱스가 해당 범위를 벗어났습니다.")
-
-            for currentChild in self.__children:
-                if currentChild.name is _index:
-                    return currentChild
-
-            return None
-        else:
-            raise ValueError(f"[Oops!] 인덱스가 틀렸거나, 해당 인덱스에 자식 Transform이 존재하지 않습니다. " +
-                             f"검색한 인덱스는 {str(_index)}였습니다.")
-        # endregion
+    # 자식들의 월드 좌표 갱신
+    def __UpdateChildren(self):
+        for child in self.__children:
+            child.position  = child.localPosition + self.__position
+            child.scale     = Vector2(child.localScale.x * self.__scale.x, child.localScale.y * self.__scale.y)
+            child.rotation  = child.localRotation + self.__rotation
