d697b74ba9eb88461624050f45a199512f3abe34
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-10-09 23:08:06 +0900

    feat : Animation 로직 추가

--- Scripts/Frameworks/Core/Components/Animator.py
+++ Scripts/Frameworks/Core/Components/Animator.py
@@ -5,10 +5,16 @@ from pico2d import Image
 
 from Core.Components.Behavior import Behavior
 from Core.Components.GameObject import GameObject
+from Core.Components.SpriteRenderer import SpriteRenderer
 from Core.Utilities.ResourceManagement import ResourceLoader
 
 class Animation:
-    def __init__(self, _isLoop: bool = True):
+    def __init__(self,
+                 _animator: 'Animator',
+                 _isLoop: bool = True):
+        self.__animator: 'Animator'             = _animator
+        self.__spriteRenderer: SpriteRenderer   = _animator.gameObject.GetComponent(SpriteRenderer)
+
         self.__isLoop: bool                 = _isLoop   # 해당 스프라이트의 루프 여부.
         self.__isEnd: bool                  = False     # 해당 스프라이트의 루프 종료 여부.
         self.__currentAnimateTime: float    = 0.1       # 해당 스프라이트의 애니메이션 시간.
@@ -60,15 +66,19 @@ class Animation:
         self.__animationDeltaTime +=  _deltaTime
 
         if self.__animationDeltaTime > self.__currentAnimateTime:
-            self.__animationDeltaTime -= - self.__currentAnimateTime
+            self.__animationDeltaTime -= self.__currentAnimateTime
             self.__currentSpriteIndex += 1
+            self.__spriteRenderer.sprite = self.__sprites[self.__currentSpriteIndex]
 
             if self.__currentSpriteIndex >= self.spritesLength:
+                self.__isEnd = True
+
                 if self.isLoop:
                     self.__currentSpriteIndex = 0
+                    self.__isEnd = False
                 else:
                     self.__currentSpriteIndex = self.__currentSpriteIndex - 1
-                    self.__isEnd = True
+                    return
 
 class Animator(Behavior):
     def __init__(self, _actor: GameObject):
