977626436ecb6a33d2138c96260f4968f6bacecb
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-10-12 00:22:35 +0900

    feat : Behavior Controller 완성

--- Scripts/Core/Objects/GameObject.py
+++ Scripts/Core/Objects/GameObject.py
@@ -11,6 +11,16 @@ class GameObject(Object):
         super().__init__()
         self.__isActiveSelf: bool = True
 
+        from Core.Behaviors.Behavior import BehaviorController
+        from Core.Components.Component import ComponentController
+
+        self.__componentController: ComponentController = ComponentController(self)
+        self.__behaviorController: BehaviorController   = BehaviorController(self)
+
+        from Core.Components.Transform import Transform
+
+        self.__transform: Transform = self.__componentController.AddComponent(Transform)
+
     # region Properties
     @property
     def gameObject(self) -> GameObject:
@@ -21,20 +31,9 @@ class GameObject(Object):
         return self
 
     @property
-    def name(self) -> str:
-        """
-        해당 Game Object의 이름을 반환합니다.
-        :return: 해당 Game Object의 이름.
-        """
-        return self.__name
+    def transform(self) -> 'Transform':
+        return self.__transform
 
-    @name.setter
-    def name(self, _name) -> None:
-        """
-        해당 Game Object의 이름을 설정합니다.
-        :param _name: 해당 Game Object의 새로운 이름.
-        """
-        self.__name = _name
     @property
     def isActiveSelf(self) -> bool:
         """
@@ -52,6 +51,7 @@ class GameObject(Object):
         return self.__isActiveSelf
     # endregion
     # region Life-Cycle
+    @abstractmethod
     def Start(self) -> None:
         """
         해당 Game Object가 탄생했을 때 한번 실행됩니다.
@@ -60,25 +60,33 @@ class GameObject(Object):
         super().Start()
 
     @abstractmethod
-    def Update(self) -> None:
+    def Update(self, _deltaTime: float) -> None:
         """
         해당 Game Object의 상태를 매 프레임 갱신합니다.
         (※ 게임 루프 내에서 매 프레임 호출됩니다.)
+        :param _deltaTime: 이전 프레임과 현재 프레임 사이의 시간 간격(초 단위).
         """
-        pass
+        self.__behaviorController.Update(_deltaTime)
 
     @abstractmethod
-    def FixedUpdate(self) -> None:
+    def FixedUpdate(self, _fixedDeltaTime: float) -> None:
         """
-        해당 Game Object의 물리 연산과 같은 고정된 주기의 업데이트를 처리합니다.
+        해당 Game Object의 물리 연산과 같은 고정된 주기의 업데이트를 처리합니다.\n
         (※ 물리 연산이나 시간에 의존적인 처리는 이 메서드에서 처리됩니다.)
+        :param _fixedDeltaTime: 고정 업데이트 주기 동안의 시간 간격(초 단위).
         """
-        pass
+        self.__componentController.FixedUpdate()
+        self.__behaviorController.FixedUpdate(_fixedDeltaTime)
 
+    @abstractmethod
     def OnDestroy(self) -> None:
         """
-        해당 Game Object가 파괴될 때 한번 실행됩니다.
+        해당 Game Object가 파괴될 때 한번 실행됩니다.\n
         (※ 해당 Game Object를 관리하는 Controller/Manager로부터 삭제된 시점을 의미합니다. 완전한 삭제는 '가비지 컬렉터'에 의존합니다.)
         """
         super().OnDestroy()
+
+        map(lambda component: Object.Destroy(component), self.__componentController)    # 컴포넌트 파괴
+        map(lambda behavior: Object.Destroy(behavior), self.__behaviorController)       # 행동 파괴
+        map(lambda child: Object.Destroy(child), self.transform.children)               # 자식 파괴
     # endregion
