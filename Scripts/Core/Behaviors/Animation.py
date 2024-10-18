9cf74125a78700798efa506db3f37cf0273c1a76
WHTBUM <bum2680@naver.com>
2024-10-18 13:38:18 +0900

    feat : 애니메이션 필드 추가

--- Scripts/Core/Behaviors/Animation.py
+++ Scripts/Core/Behaviors/Animation.py
@@ -3,15 +3,35 @@ from typing import Dict
 from Core.Behaviors.Behavior import Behavior
 
 class Animation:
-    pass
+    """
+    각종 오브젝트의 애니메이션을 구현합니다.
+    """
+    def __init__(self):
+        self.__isLoop: bool = False     # 애니메이션 루프 여부.
+        self.__isEnd: bool  = False     # 애니메이션 종료 여부.
+
+    @property
+    def isLoop(self) -> bool:
+        return self.__isLoop
+
+    @isLoop.setter
+    def isLoop(self, _value: bool) -> None:
+        self.__isLoop = _value
+
+    @property
+    def isEnd(self) -> bool:
+        return self.__isEnd
 
 class Animator(Behavior):
+    """
+    애니메이션을 관리하고 재생합니다.
+    """
     def __init__(self, _actor: 'GameObject'):
         super().__init__(_actor)
 
-        self.__animation: Dict[str, Animation] = {};
-        self.__animationDeltaTime: float = 0;
-        self.__animationDeltaTime
+        self.__animations: Dict[str, Animation]     = {}    # 관리 중인 애니메이션들.
+        self.__animationDeltaTime: float            = 0.1   # 애니메이션 제어 시간.
+        self.__currentDeltaTime: float              = 0.0   # 현재 진행된 애니메이션 시간.
 
     def Start(self):
         pass
