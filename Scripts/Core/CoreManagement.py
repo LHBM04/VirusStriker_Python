63971c9f653e716ac6d6979956525043f88d9304
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-10-21 00:51:42 +0900

    feat : Setting.json을 통한 윈도우 설정 기능 추가

--- Scripts/Core/CoreManagement.py
+++ Scripts/Core/CoreManagement.py
@@ -1,4 +1,3 @@
-import time
 from ctypes import *
 from json import *
 from typing import *
@@ -14,9 +13,12 @@ from Level.SceneManagement import SceneManager
 class Core(metaclass = Singleton):
     def __init__(self):
         # region Const Field
+        self.__GAME_TITLE: str              = "Virus Striker"
+        self.__GAME_VERSION: str            = "Alpha 1.0"
+
         self.__DEFAULT_WINDOW_WIDTH: int    = 1280
         self.__DEFAULT_WINDOW_HEIGHT: int   = 720
-        self.__DEFAULT_WINDOW_STATE: int    = SDL_WINDOW_SHOWN
+        self.__DEFAULT_WINDOW_STATE: int    = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
         self.__DEFAULT_WINDOW_SYNC: int     = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
         # endregion
 
@@ -42,8 +44,11 @@ class Core(metaclass = Singleton):
         return self.__windowHeight
 
     def Initialize(self) -> None:
-        settingFile: Path = Path(r"Resources\Setting\Setting.json")
+        settingFile: Path = Path(r"Resources\Setting.json")
+        print("Setting.json을 찾고 있습니다.")
+
         if not settingFile.exists() or not settingFile.is_file():
+            print("Setting.json을 찾지 못했습니다. 기본 설정으로 기동합니다...")
             self.__windowWidth     = self.__DEFAULT_WINDOW_WIDTH
             self.__windowHeight    = self.__DEFAULT_WINDOW_HEIGHT
             self.__screenState     = self.__DEFAULT_WINDOW_STATE
@@ -53,14 +58,15 @@ class Core(metaclass = Singleton):
             with settingFile.open('r') as file:
                 settingData: Dict[str, bytes] = load(file)
 
+                print("Setting.json을 찾았습니다 해당 설정으로 기동합니다...")
                 self.__windowWidth  = settingData.get("Window Width")
                 self.__windowHeight = settingData.get('Window Height')
-                self.__screenState  = settingData.get('Fullscreen')
-                self.__syncState    = settingData.get('Sync')
+                self.__screenState  = settingData.get('Screen State')
+                self.__syncState    = settingData.get('Sync State')
 
         SDL_Init(SDL_INIT_EVERYTHING)
         self.__windowHandle = SDL_CreateWindow(
-            f"Virus Striker {self.__windowWidth} x {self.__windowHeight}".encode('utf-8'),
+            f"{self.__GAME_TITLE} Ver. {self.__GAME_VERSION} ({self.__windowWidth} x {self.__windowHeight})".encode('utf-8'),
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
             self.__windowWidth,
@@ -73,10 +79,14 @@ class Core(metaclass = Singleton):
             self.__syncState
         )
 
+        # Fallback
+        if self.__rendererHandle is None:
+            self.__rendererHandle = SDL_CreateRenderer(self.__windowHandle, -1, SDL_RENDERER_SOFTWARE)
+
     def Run(self) -> None:
         self.__isRunning = True
 
-        previousTime: float     = time.time()
+        previousTime: int       = SDL_GetTicks()
         fixedUpdateTime: float  = 1.0 / 50.0
         fixedDeltaTime: float   = 0.0
 
@@ -87,10 +97,10 @@ class Core(metaclass = Singleton):
             SDL_RenderClear(self.__rendererHandle)
 
             if SceneManager().isResetDeltaTime:
-                previousTime = time.time()
+                previousTime = SDL_GetTicks()
 
-            currentTime: float  = time.time()
-            deltaTime:float     = currentTime - previousTime
+            currentTime: int    = SDL_GetTicks()
+            deltaTime: float    = (currentTime - previousTime) / 1000.0
 
             previousTime = currentTime
 
@@ -112,6 +122,9 @@ class Core(metaclass = Singleton):
         SDL_DestroyWindow(self.__windowHandle)
         SDL_Quit()
 
+    def Quit(self) -> None:
+        self.__isRunning = False
+
     def ProceedEvent(self) -> SDL_Event:
         event: SDL_Event = SDL_Event()
         while SDL_PollEvent(byref(event)):
@@ -123,5 +136,12 @@ class Core(metaclass = Singleton):
 
         return event
 
-    def ResizeWindow(self) -> None:
-        pass
+    def ResizeWindow(self, _width: int, _height: int) -> None:
+        self.__windowWidth  = _width
+        self.__windowHeight = _height
+
+        SDL_SetWindowSize(self.__windowWidth, self.__windowWidth)
+        SDL_SetWindowTitle(f"{self.__GAME_TITLE} Ver. {self.__GAME_VERSION} ({self.__windowWidth} x {self.__windowHeight})".encode('utf-8'))
+
+        SDL_RenderClear(self.__rendererHandle)
+        SDL_RenderPresent(self.__rendererHandle)
