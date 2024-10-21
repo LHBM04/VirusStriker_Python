3c4838f6233f64fe061977dec9c61a2bb2fe5c2e
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-10-22 02:10:45 +0900

    Update ResourceManagement.py
    - 리소스 로딩 테스트

--- Scripts/Core/Utilities/ResourceManagement.py
+++ Scripts/Core/Utilities/ResourceManagement.py
@@ -46,12 +46,14 @@ class Sprite:
 
         SDL_RenderCopyEx(SystemManager().rendererHandle, self.__texture, None, rect, degrees(-_rotate), None, flipFlag)
 
+@final
 class ResourceManager(metaclass = Singleton):
+    # TODO: 멀티 스레딩으로 리소스 긁어오는 거 고려해보자.
     def __init__(self):
-        self.__textureBank: Dict[str, SDL_Texture]  = { }
-        self.__bgmBank: Dict[str, Mix_Music]        = { }
-        self.__sfxBank: Dict[str, Mix_Music]        = { }
-        self.__ttfBank: Dict[str, TTF_Font]         = { }
+        self.__textureBank: Dict[str, List[SDL_Texture]]    = { }
+        self.__bgmBank: Dict[str, Mix_Music]                = { }
+        self.__sfxBank: Dict[str, Mix_Music]                = { }
+        self.__ttfBank: Dict[str, TTF_Font]                 = { }
 
         self.__textureResourcePath: Path    = Path(r"Resources\Sprites")
         self.__textureResourceSuffix: str   = "*.png"
@@ -71,24 +73,24 @@ class ResourceManager(metaclass = Singleton):
         IMG_Quit()
 
     def Initialize(self) -> None:
-        self.LoadImages()
+        self.LoadTextures()
         self.LoadBGM()
         self.LoadSFX()
         self.LoadFont()
 
-    def LoadImages(self) -> None:
-        if not self.__spriteResourcePath.exists() or not self.__spriteResourcePath.is_dir():
+    def LoadTextures(self) -> None:
+        if not self.__textureResourcePath.exists() or not self.__textureResourcePath.is_dir():
             pass
 
-        for filePath in self.__spriteResourcePath.rglob(self.__spriteResourceSuffix):
+        for filePath in self.__textureResourcePath.rglob(self.__textureResourceSuffix):
             if not filePath.exists() or not filePath.is_file():
                 raise IOError
 
-            if filePath.name not in self.__spriteBank:
-                self.__spriteBank[filePath.name] = []
+            if filePath.name not in self.__textureBank:
+                self.__textureBank[filePath.name] = []
 
             print(str(filePath))
-            self.__spriteBank[filePath.name].append(load_image(str(filePath)))
+            self.__textureBank[filePath.name].append(IMG_LoadTexture(SystemManager().rendererHandle, str(filePath).encode("UTF-8")))
 
     def LoadBGM(self) -> None:
         if not self.__bgmResourcePath.exists() or not self.__bgmResourcePath.is_dir():
@@ -99,7 +101,7 @@ class ResourceManager(metaclass = Singleton):
                 raise IOError
 
             print(str(filePath))
-            self.__bgmBank[filePath.name] = load_music(str(filePath))
+            self.__bgmBank[filePath.name] = Mix_LoadMUS(str(filePath).encode("UTF-8"))
 
     def LoadSFX(self) -> None:
         if not self.__sfxResourcePath.exists() or not self.__sfxResourcePath.is_dir():
@@ -110,7 +112,7 @@ class ResourceManager(metaclass = Singleton):
                 raise IOError
 
             print(str(filePath))
-            self.__sfxBank[filePath.name] = load_music(str(filePath))
+            self.__sfxBank[filePath.name] = Mix_LoadMUS(str(filePath).encode("UTF-8"))
 
     def LoadFont(self) -> None:
         if not self.__fontResourcePath.exists() or not self.__fontResourcePath.is_dir():
@@ -121,4 +123,5 @@ class ResourceManager(metaclass = Singleton):
                 raise IOError
 
             print(str(filePath))
-            self.__fontBank[filePath.name] = load_font(str(filePath), 20)
+            # TODO: Font 파일 읽어오기
+            # self.__fontBank[filePath.name] = load_font(str(filePath), 20)
