438d24fc9a96ae5cd58b7b935b0f9d3290571a95
ASOBIBA_ <86977637+LHBM04@users.noreply.github.com>
2024-10-22 01:58:23 +0900

    Update AudioManagement.py
    - 클래스 서피스만 구성.

--- Scripts/Level/AudioManagement.py
+++ Scripts/Level/AudioManagement.py
@@ -1,1 +1,19 @@
+from typing import final
 
+from Core.Utilities.Singleton import Singleton
+
+@final
+class AudioChannel:
+    def __init__(self):
+        ...
+
+    def __del__(self):
+        ...
+
+@final
+class AudioManager(metaclass = Singleton):
+    def __init__(self):
+        ...
+
+    def __del__(self):
+        ...
